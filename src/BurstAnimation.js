653455909f67499a45b4b8ca7ac3287071b24966
Mohammed labied <m.mohammed.labied@gmail.com>
2020-03-13 17:46:17 +0100

    add list of component

--- src/BurstAnimation.js
+++ src/BurstAnimation.js
@@ -10,63 +10,127 @@ import {
 
 const { width, height } = Dimensions.get("window");
 const Intial_Height = height / 4;
-export default class BurstAnimation extends Component {
+let yPos = 2;
+let heigSecondViewLeft;
+class BurstAnimation extends Component {
   constructor(props) {
     super(props);
     this.state = {
       animationScroll: new Animated.Value(0)
     };
   }
-  getInerpolationColor = i => {
-    const inputRange = [(i - 1) * width, i * width, (i + 1) * width];
-    const outputRange = [20, 100, 200];
+  getInerpolationColor = () => {
+    const inputRange = [0, width];
+    const outputRange = [width, 20];
     return this.state.animationScroll.interpolate({
       inputRange,
       outputRange,
       extrapolate: "clamp"
     });
   };
+  getInerpolationTop = () => {
+    console.log("dakhl lfuction : ", -this.props.index * yPos);
+    const inputRange = [0, width];
+    const outputRange = [0, -this.props.index * yPos];
+    return this.state.animationScroll.interpolate({
+      inputRange,
+      outputRange,
+      extrapolate: "clamp"
+    });
+  };
+
+  getInterpolateHeigt = () => {
+    const inputRange = [0, width];
+    const outputRange = [Intial_Height, height - 40];
+    return this.state.animationScroll.interpolate({
+      inputRange,
+      outputRange,
+      extrapolate: "clamp"
+    });
+  };
+  find_dimesions(layout) {
+    const { x, y, width, height } = layout;
+    this.xPos = x;
+    (yPos = y), (this.viewWidth = width), (this.viewHeight = height);
+    console.log("y : ", yPos);
+    heigSecondViewLeft = {
+      left: this.getInerpolationColor(),
+      height: this.getInterpolateHeigt(),
+      top: this.getInerpolationTop()
+    };
+    this.forceUpdate();
+  }
   render() {
-    const heigSecondView = {
-      height: this.getInerpolationColor(0)
+    heigSecondViewLeft = {
+      left: this.getInerpolationColor(),
+      height: this.getInterpolateHeigt(),
+      top: this.getInerpolationTop()
     };
     return (
-      <View style={styles.container}>
-        <View style={{ height: Intial_Height }}>
-          <Animated.ScrollView
-            style={styles.scrollContainer}
-            showsHorizontalScrollIndicator={false}
-            pagingEnabled
-            scrollEventThrottle={16}
-            horizontal
-            onScroll={Animated.event([
-              {
-                nativeEvent: {
-                  contentOffset: {
-                    x: this.state.animationScroll
-                  }
+      <View
+        style={{ height: Intial_Height }}
+        onLayout={event => {
+          this.find_dimesions(event.nativeEvent.layout);
+        }}
+      >
+        <Animated.ScrollView
+          style={styles.scrollContainer}
+          showsHorizontalScrollIndicator={false}
+          pagingEnabled
+          scrollEventThrottle={16}
+          horizontal
+          onScroll={Animated.event([
+            {
+              nativeEvent: {
+                contentOffset: {
+                  x: this.state.animationScroll
                 }
               }
-            ])}
-          >
-            <View style={{ width: width, backgroundColor: "green" }}>
-              <Text>hello</Text>
-            </View>
-            <Animated.View
-              style={
-                ([
-                  {
-                    width: width,
-                    backgroundColor: "yellow"
-                  }
-                ],
-                heigSecondView)
+            }
+          ])}
+        >
+          <View style={{ width: width, backgroundColor: "green" }}>
+            <Text>hello</Text>
+          </View>
+          <Animated.View
+            style={[
+              {
+                width: width,
+                backgroundColor: "yellow"
               }
-            >
-              <Text>hello</Text>
-            </Animated.View>
-          </Animated.ScrollView>
-        </View>
+            ]}
+          >
+            <Text>hello</Text>
+          </Animated.View>
+        </Animated.ScrollView>
+        <Animated.View
+          style={[
+            {
+              backgroundColor: "gray",
+              width: width - 40,
+              marginTop: 5,
+              position: "absolute",
+              borderRadius: 30,
+              marginRight: 20
+
+              // overflow: "visible"
+            },
+            heigSecondViewLeft
+          ]}
+        ></Animated.View>
+      </View>
+    );
+  }
+}
+
+export default class BurstAnimationContainer extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <BurstAnimation index={0}></BurstAnimation>
+        <BurstAnimation index={1}></BurstAnimation>
+        <BurstAnimation index={1}></BurstAnimation>
+        <BurstAnimation index={1}></BurstAnimation>
       </View>
     );
   }
